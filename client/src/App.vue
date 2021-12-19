40faae7a8508ead281e06a4bfbec2679baeb30aa
honeeee <knn92@hanmail.net>
2021-12-19 15:51:23 +0900

    커밋1

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,32 @@
-<script setup lang="ts">
-import {ref} from 'vue';
-
-const users2 = ref([
-  {"id": 1, "name": "홍길동", "age": 22},
-  {"id": 2, "name": "강감찬123", "age": 43},
-  {"name": "바붕이", "age": 99, "id": 3},
-]);
+<script >
+import { ref } from '@vue/reactivity';
 
 // fetch ~ promise
 let users3 = ref<any[]>([]);
 fetch('http://localhost:3000/users')
-    .then(response => response.json())
-    .then(json => users3.value = json);
+  .then(response => response.json())
+  .then(json => users3.value = json);
 
-const promise = fetch('http://localhost:3000/users');
-promise
-    .then(function(response) {
-      return response.json()
-    })
-    .then(json => users3.value = json);
+const promise = fetch('http://localhost:3000/users');//패치가 실행되서 프라미스가 리턴되면
+promise  //그게 오면 
+  .then(response => response.json())//json으로 호출하면 json으로 데이터 줌
+  // .then(function(response) {
+  //    return response.json()
+  //  }) 
+  .then(json => users3.value = json);
+//패치가 프라미스를 리턴
+//데이터를 받으면 
+
+// ajax
+// $.ajax( success ~~ ) 문법이 위에처럼 바뀐거임
+
 
 // async ~ await
-let users = ref<any[]>([]);
-
+let users3 = ref<any[]>([]);
 async function loadUsers() {
   const response = await fetch('http://localhost:3000/users');
-  users.value = await response.json();
+  return await response.json();
+
 }
 
 loadUsers();
@@ -39,11 +40,15 @@ loadUsers();
 
 <style>
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 3rem;
+  padding: 0 2rem;
+  background: #38ada9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
