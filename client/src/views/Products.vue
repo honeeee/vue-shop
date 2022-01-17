11ca0294f96615646030d3137af02340b6d59658
honeeee <knn92@hanmail.net>
2022-01-17 19:34:46 +0900

    제품검색추가

--- client/src/views/Products.vue
+++ client/src/views/Products.vue
@@ -2,12 +2,13 @@
 import { onMounted, ref, computed } from 'vue'
 import {addItemToCart, deleteCartItem, getProduct} from '../service/products'
 import { useStore } from 'vuex'
+import axios from 'axios';
 // 상세 페이지까지 만들었음
 // 스크림트에서 라우터 쓰는 방법
 // useRouter: link로 보낼때, 뒤로가기앞으로 가기 등등 여러 기능있음
+// useRoute: 현재 이 페이지의 정보를 담고있는 객체, 변경을 감지
 // fullpath
 // query =내가 보낸 값들이 들어있음
-// useRoute: 현재 이 페이지의 정보를 담고있는 객체, 변경을 감지
 
 // 상세페이지-넘어온 id를 받아서-tsalcuTek. const route = UseRoute()-const id = route.query.id
 /** 
@@ -18,6 +19,8 @@ import { useStore } from 'vuex'
 const products = ref<any[]>([])
 const store = useStore();
 const cart = computed(()=> store.state.cart)
+// const searchText = ref<string>();
+const searchText = ref("");
 
 async function loadProducts(){
     products.value = await getProduct()
@@ -37,43 +40,21 @@ async function toggleCart(id:number){
     store.commit('toggleItem',id);
 }
 
-loadProducts()
-/*
-// export default{
-//     props:{
-//     },
-//     setup() {
-        
-//         const addToCart() {
-//             this.$store.dispatch("addProductToCart", {
-//                 product: this.product,
-//                 quantity: 1
-//             });
+async function searchProducts(){
+    await loadProducts();
+    console.log(searchText.value);
+    const search = computed(() => {
+        return products.value.filter( poke => {
+            return poke.name.toLowerCase().includes(searchText.value);
+        })
+    })
+    return products.value = search.value;
+}
 
-//         }
 
-//         return{
-//             addToCart,
-//         }
-//     }
-// }
-// export default {
-//   name: "ProductPage",
-//   data() {
-//     const index = this.$route.params.contentId;
-//     return {
-//       product: this.$store.state.db[index]
-//     };
-//   },
-//   methods: {
-//     addToCart(product) {
-//       this.$store.dispatch("addToCart", product);
-//     },
-    
-//   },
-// };
 
-*/
+loadProducts();
+
 </script>
 
 <template>
@@ -96,8 +77,10 @@ loadProducts()
                                 <path d="M7 11H17V13H7zM4 7H20V9H4zM10 15H14V17H10z" />
                             </svg>
                         </a>
+                        <input type="text" placeholder="제품명 검색" v-model="searchText" name="searchText" @keyup.enter="searchProducts()"
+                        class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
 
-                        <a class="pl-3 inline-block no-underline hover:text-black" href="#">
+                        <a class="pl-3 inline-block no-underline hover:text-black" @click.prevent="searchProducts()">
                             <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                 <path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z" />
                             </svg>
@@ -106,22 +89,24 @@ loadProducts()
                     </div>
                 </div>
             </nav>
-
+            
             <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col" 
                 v-for="prd in products" :key="prd.id">
-                <router-link :to="`/product?id=${prd.id}`">
-                    <img class="hover:grow hover:shadow-lg" src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80">
-                    <div class="pt-3 flex items-center justify-between">
-                        <p class="">{{prd.name}}</p>
-                        <svg class="h-6 w-6 fill-current text-gray-500"
-                            :class="{'text-red-500': cart.includes(prd.id)}"
-                            @click.prevent="toggleCart(prd.id)"
-                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
-                        </svg>
-                    </div>
-                    <p class="pt-1 text-gray-900">{{prd.price}}</p>
-                </router-link>
+                <!-- <div v-if="prd.name.includes(searchText)"> -->
+                    <router-link :to="`/product?id=${prd.id}`">
+                        <img class="hover:grow hover:shadow-lg" src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80">
+                        <div class="pt-3 flex items-center justify-between">
+                            <p class="">{{prd.name}}</p>
+                            <svg class="h-6 w-6 fill-current text-gray-500"
+                                :class="{'text-red-500': cart.includes(prd.id)}"
+                                @click.prevent="toggleCart(prd.id)"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
+                            </svg>
+                        </div>
+                        <p class="pt-1 text-gray-900">{{prd.price}}</p>
+                    </router-link>
+                <!-- </div> -->
             </div>
 
         </div>
